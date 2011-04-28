f5a50df446b6b02ab7e76162c7bd9cd5b2113160
lestrrat <lestrrat+github@gmail.com>
2011-04-28 12:57:35 +0900

    add docs

--- lib/MIME/Magic.pm
+++ lib/MIME/Magic.pm
@@ -41,6 +41,11 @@ sub parse_magic {
     $self;
 }
 
+sub add_magic {
+    my ($self, $magic) = @_;
+    unshift @{ $self->magic }, $magic;
+}
+
 sub guess_mime {
     my ($self, $file) = @_;
     open my $fh, '<', $file or die;
@@ -62,6 +67,38 @@ __END__
 
 MIME::Magic - Alternate MIME-Magic Guessing
 
+=head1 SYNOPSIS
+
+    use MIME::Magic;
+
+    # use the default MIME magic map that comes with MIME::Magic
+    # (see MIME::Magic::Default)
+    my $m = MIME::Magic->new;
+    my $type = $m->guess_mime( $filename );
+
+    # use your own magic map ( recommended if you expect a limited set
+    # of input, as it will be faster )
+    my $m = MIME::Magic->new;
+    $m->parse_magic( $filename );
+    $m->guess_mime( $another_filename );
+
+    # define your own magic on the fly (will be prepended to the list)
+    my $magic = MIME::Magic::Entry->new;
+    $magic->add_rule( 
+        byte => ...,
+        type => ...,
+        content => ...,
+        mime => ...,
+        encoding => ...,
+        mask => ...,
+    );
+    $m->add_magic( $magic );
+
+=head1 DESCRIPTION
+
+This module attemmpts to do the equivalent of mod_mime_magic from Apache
+httpd server.
+
 =head1 TODO
 
 =over 4
@@ -72,6 +109,16 @@ MIME::Magic - Alternate MIME-Magic Guessing
 
 =item add more test cases
 
+=item write another XS package for alternate backend
+
 =back
+
+=head1 AUTHOR
+
+Copyright (C) 2011 by Daisuke Maki
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.0 or,
+at your option, any later version of Perl 5 you may have available.
 
 =cut
